bfd59670ef30a3fb920155ffd55dbd3c0293b790
Pedro Rabis <rabis.pedro@gmail.com>
2023-09-12 17:01:47 -0300

    fix: ajust enemy wall_detector

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 700.0
 const JUMP_VELOCITY = -400.0
 
@@ -11,21 +10,20 @@ var direction := -1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	if wall_detector.is_colliding():
 		direction *= -1
 		wall_detector.scale.x *= -1
+		print("Colisao detectada")
 
 	if direction == 1:
 		texture.flip_h = true
 	else:
 		texture.flip_h = false
-		
-	velocity.x = direction * SPEED *delta
+
+	velocity.x = direction * SPEED * delta
 
 	move_and_slide()
