c6a78c6d454660c040e795739494115bfba19c5e
Marcos Lima <araujolima728@gmail.com>
2024-03-12 12:07:49 -0300

    Bug: Ep 21 - Inimigo Duplo + Conceito Básico de Classe Godot 4.0 - Criando um Jogo de Plataforma 2D

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,8 +1,10 @@
-class_name Enemy
 extends CharacterBody2D
+class_name EnemyBase
 
 const SPEED: float = 700.0
 const JUMP_VELOCITY: float = -400.0
+
+@onready var anim := $anim
 
 @export_category("Properties")
 @export_range(0, 500, 1) var enemy_score: int = 100
@@ -10,13 +12,27 @@ const JUMP_VELOCITY: float = -400.0
 var direction: int = -1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var wall_detector: RayCast2D = $wall_detector
-@onready var texture: Sprite2D = $texture
+var can_spawn = false
+var spawn_instance : PackedScene = null
+var spawn_instance_position
 
-func _physics_process(delta: float) -> void:
+var wall_detector
+var texture
+
+
+func _apply_gravity(delta) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
+	return
+	
+
+func movement(delta) -> void:
+	velocity.x = direction * SPEED * delta
+	move_and_slide()
+	return
+
+func flip_direction() -> void:
 	if wall_detector.is_colliding():
 		direction *= -1
 		wall_detector.scale.x *= -1
@@ -25,14 +41,25 @@ func _physics_process(delta: float) -> void:
 		texture.flip_h = true
 	else:
 		texture.flip_h = false
-	
-	velocity.x = direction * SPEED * delta
-	
-	move_and_slide()
 	return
+		
+func kill_ground_enemy(anim_name: StringName) -> void:
+	kill_and_score()
 
-func _on_anim_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "hurt":
-		Globals.score += enemy_score
+func kill_air_enemy(anim_name: StringName) -> void:
+	kill_and_score()
+
+func kill_and_score():
+	Globals.score += enemy_score
+	if can_spawn:
+		spawn_new_enemy()
+		get_parent().queue_free()
+	else:
 		queue_free()
-	return
+
+func spawn_new_enemy():
+	var instance_scene = spawn_instance.instantiate()
+	get_tree().root.add_child(instance_scene)
+	instance_scene.global_position = spawn_instance_position.global_position
+
+
--- scripts/rocket_cherry.gd
+++ scripts/rocket_cherry.gd
@@ -1,17 +1,14 @@
 class_name RocketCherry
-extends CharacterBody2D
+extends EnemyBase
 
 @export_category("Properties")
-@export_range(0, 500, 1) var enemy_score: int = 150
+@onready var spawn_enemy = $"../SpawnEnemy"
+@onready var animation: AnimatedSprite2D = $anim
 
-@onready var animation: AnimatedSprite2D = $Anim
+func _ready():
+	spawn_instance = preload("res://actors/cherry.tscn")
+	spawn_instance_position = spawn_enemy
+	can_spawn = true
+	anim.animation_finished.connect(kill_air_enemy)
 
-func _on_hitbox_body_entered(_body: Node2D) -> void:
-	animation.play("hurt")
-	return
 
-func _on_anim_animation_finished() -> void:
-	if animation.animation == "hurt":
-		queue_free()
-		Globals.score += enemy_score
-	return
