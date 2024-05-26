33bc7789419c64171fba51b1d66e32877c3caa76
Marcos Lima <araujolima728@gmail.com>
2024-05-26 10:44:26 -0300

    fix Ep 27 - Title Screen (Como fazer a tela inicial) na Godot 4 - Criando um Jogo de Plataforma 2D

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -15,7 +15,7 @@ func _on_start_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/world_01.tscn")
 	return
 
-func _on_credits_btn_2_pressed() -> void:
+func _on_credits_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://prefabs/credits_screen.tscn")
 	return
 
@@ -23,7 +23,3 @@ func _on_credits_btn_2_pressed() -> void:
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
 	return
-
-
-func _on_credits_btn_pressed():
-	pass # Replace with function body.
